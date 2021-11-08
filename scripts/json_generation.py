32f0b6d7c6515cd26aa2e921ed3ddecc6180cfa4
Emma Andrews <eandrews258@gmail.com>
2021-11-07 19:13:45 -0500

    updated json generation

--- scripts/json_generation.py
+++ scripts/json_generation.py
@@ -16,7 +16,8 @@ for index, row in df.iterrows():
         'stop_name': "", 
         'stop_id': "",
         'stop_desc': "",
-        'route_id': []
+        'route_id': [],
+        'route_id_times': []
     }
 
     stop = row['stop_id']
@@ -37,8 +38,32 @@ for index, row in df.iterrows():
     x['route_id'].sort(key=int)
     d.append(x)
 
-print(df)
-print(df.at[0,'stop_id'])
+keys = []
 
-with open('stop_name.json', 'w', encoding='utf-8') as f:
+for i in range(len(d)):
+    entry = d[i]
+    for j in range(len(entry['route_id'])):
+        keys.append(str(entry['stop_id']) + "_" + str(entry['route_id'][j]))
+
+d2 = {key: [] for key in keys}
+
+time_df = pd.read_csv("../csvs/timings.csv")
+
+for index, row in time_df.iterrows():
+    k = str(row['stop_id']) + "_" + str(row['route_id'])
+    if k in d2:
+        d2[k].append(row['arrival_time'])
+
+for i in range(len(d)):
+    entry = d[i]
+    for j in range(len(entry['route_id'])):
+        k = str(entry['stop_id']) + "_" + str(entry['route_id'][j])
+        if k in d2:
+            d2[k].sort()
+            entry['route_id_times'].append(d2[k])
+
+with open('stop_name_time.json', 'w', encoding='utf-8') as f:
     json.dump(d, f, ensure_ascii=False, indent=4)
+
+with open('stop_name_time_noformat.json', 'w', encoding='utf-8') as f:
+    json.dump(d, f)
